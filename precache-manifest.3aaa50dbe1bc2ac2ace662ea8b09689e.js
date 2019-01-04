f0cc98cc4e49841f365afd3478cec85bc5c51bbc
Lucas Suarez <lucas.suarez.utepsa@gmail.com>
2019-01-04 09:47:15 -0400

    Updates

---
+++ precache-manifest.3aaa50dbe1bc2ac2ace662ea8b09689e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9ca4458ea1bf031cf1818fb45d294dc",
+    "url": "/minnavigation/static/media/sagaya-abdulhafeez-1208693-unsplash.d9ca4458.jpg"
+  },
+  {
+    "revision": "3b6358992942adbf28f5",
+    "url": "/minnavigation/static/js/runtime~main.3b635899.js"
+  },
+  {
+    "revision": "b0d08f24d6006a7f923b",
+    "url": "/minnavigation/static/js/main.b0d08f24.chunk.js"
+  },
+  {
+    "revision": "272d1c48e52789c83abc",
+    "url": "/minnavigation/static/js/1.272d1c48.chunk.js"
+  },
+  {
+    "revision": "b0d08f24d6006a7f923b",
+    "url": "/minnavigation/static/css/main.4fd4a95f.chunk.css"
+  },
+  {
+    "revision": "eaac429b9eadcb7a2da7a14cadcec2bc",
+    "url": "/minnavigation/index.html"
+  }
+];
